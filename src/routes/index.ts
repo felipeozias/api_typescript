5a0ecd2d7c385623275a30f5de1cae46e2987d3f
LucasCscBks <luke100191@gmail.com>
2022-11-17 17:25:15 -0300

    add route delete team_id and update get team_id and post team

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,6 +8,7 @@ router.get("/users", users.getAll);
 router.get("/teams", squads.getAll);
 router.get("/teams/:team", squads.getSquad);
 router.post("/team", squads.postSquad);
+router.delete("/team/:team_id", squads.deleteSquad);
 router.patch("/team/:team_id", squads.update);
 router.delete("/team/:team_id/member/:user_id", squads.removeMember);
 router.post("/team/:team_id/member/:user_id", squads.addMember);
