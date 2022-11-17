ad471279cbd061514471ee8cf6769b1a6a7d4901
Alan <alan.douglas.ads@gmail.com>
2022-11-17 11:43:50 -0300

    chore: remove models
    Deleted models duplicate

--- src/repositories/users.ts
+++ src/repositories/users.ts
@@ -1,6 +1,6 @@
 import connectDB from "../database/index";
 import { IStatusResponse } from "../interface";
-import IUsers from "../models/IUsers";
+import IUsers from "../interface/IUsers";
 
 export default class Users {
     private _db: connectDB;
@@ -30,30 +30,6 @@ export default class Users {
             const values = [email, false];
             const getUsers = await this._db.pool.query(
                 `SELECT username, email, first_name, last_name, password, is_admin, deleted FROM users WHERE email=$1 AND deleted=$2;`,
-                values
-            );
-
-            if (getUsers.rows.length < 1)
-                return { status: 400, response: ["email not registered"] };
-
-            const res: IStatusResponse<IUsers[]> = {
-                status: 200,
-                response: getUsers.rows,
-            };
-            return res;
-        } catch (err) {
-            const res: IStatusResponse<any> = { status: 500, response: err };
-            return res;
-        }
-    }
-
-    public async getUser(
-        email: String
-    ): Promise<IStatusResponse<IUsers[] | any>> {
-        try {
-            const values = [email, false];
-            const getUsers = await this._db.pool.query(
-                `SELECT * FROM users WHERE email=$1 AND deleted=$2;`,
                 values
             );
 
