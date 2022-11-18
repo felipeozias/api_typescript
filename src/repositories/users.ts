0f5facb61b014830fd162a9b329359291379a02d
Alan <alan.douglas.ads@gmail.com>
2022-11-18 13:25:33 -0300

    reafc: alter user type default

--- src/repositories/users.ts
+++ src/repositories/users.ts
@@ -31,7 +31,10 @@ export default class Users {
     public async getUserAuth(email: String): Promise<IResult> {
         const result: IResult = { data: [], error: "", status: 200 };
         try {
-            const data = await this._db.pool.query(`SELECT * FROM users WHERE email=$1 AND deleted=$2;`, [email, false]);
+            const data = await this._db.pool.query(
+                `SELECT * FROM users WHERE email=$1 AND deleted=$2;`,
+                [email, false]
+            );
 
             if (data.rowCount > 0) {
                 result.data = new User();
@@ -52,7 +55,10 @@ export default class Users {
             }
 
             if (data.rows[0].squad_id) {
-                const dataSqd = await this._db.pool.query(`SELECT * FROM squads WHERE id=$1;`, [data.rows[0].squad_id]);
+                const dataSqd = await this._db.pool.query(
+                    `SELECT * FROM squads WHERE id=$1;`,
+                    [data.rows[0].squad_id]
+                );
                 result.data.squad = dataSqd.rows[0];
             }
 
@@ -69,7 +75,8 @@ export default class Users {
         const now = new Date();
 
         try {
-            const query = "Update users set squad_id = null, updated_at = $2 where id = $1 RETURNING *";
+            const query =
+                "Update users set squad_id = null, updated_at = $2 where id = $1 RETURNING *";
             const values = [idUser, now];
             const data = await this._db.pool.query(query, values);
             if (data.rowCount > 0) {
@@ -102,7 +109,8 @@ export default class Users {
         const now = new Date();
 
         try {
-            const query = "Update users set squad_id = $1, updated_at = $3 where id = $2 RETURNING *";
+            const query =
+                "Update users set squad_id = $1, updated_at = $3 where id = $2 RETURNING *";
             const values = [idSquad, idUser, now];
             const data = await this._db.pool.query(query, values);
             if (data.rowCount > 0) {
@@ -133,7 +141,10 @@ export default class Users {
     async getUserById(userId: String): Promise<IResult> {
         const result: IResult = { data: [], error: "", status: 200 };
         try {
-            const data = await this._db.pool.query(`SELECT * FROM users WHERE id=$1;`, [userId]);
+            const data = await this._db.pool.query(
+                `SELECT * FROM users WHERE id=$1;`,
+                [userId]
+            );
 
             if (data.rowCount > 0) {
                 result.data = new User();
@@ -168,7 +179,14 @@ export default class Users {
             const query = `INSERT INTO "users" (username, email, first_name, last_name, "password", is_admin) 
             VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
 
-            const values = [user.username, user.email, user.firstName, user.lastName, user.password, user.type];
+            const values = [
+                user.username,
+                user.email,
+                user.firstName,
+                user.lastName,
+                user.password,
+                user.type,
+            ];
             const data = await this._db.pool.query(query, values);
 
             if (data.rowCount > 0) {
@@ -200,8 +218,15 @@ export default class Users {
         const result: IResult = { data: [], error: "", status: 200 };
 
         try {
-            let query = `UPDATE "users" SET username = $2, email = $3, first_name = $4, last_name = $5, "password" = $6, is_admin = $7 WHERE id = $1 RETURNING *`;
-            let values = [user.id, user.username, user.email, user.firstName, user.lastName, user.password, user.type];
+            let query = `UPDATE "users" SET username = $2, email = $3, first_name = $4, last_name = $5, "password" = $6 WHERE id = $1 RETURNING *`;
+            let values = [
+                user.id,
+                user.username,
+                user.email,
+                user.firstName,
+                user.lastName,
+                user.password,
+            ];
 
             const data = await this._db.pool.query(query, values);
             if (data.rowCount > 0) {
